ef2fda0c2f78ab6060318b6ca1665ab0ecf7c1ca
Nick Nazarov <luckfun37@mail.ru>
2025-11-10 22:11:19 +0300

    make servers test

--- api/api.py
+++ api/api.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse,Response
 from schemas.schemas import User
 router  = APIRouter()
 fake_db = []
+fake_servers = [{"123": "server1"}, {"1234": "server2"}, {"12345": "server3"}]
+
 @router.post('/auth/register')
 async def register(
     username: str = Form(...),
@@ -16,3 +18,15 @@ async def register(
     print("Текущая база:", fake_db)
     
     return Response(status_code=204)
+
+@router.get('/servers')
+async def get_servs():
+    return fake_servers
+
+@router.get('/servers/{id}')
+async def get_serv(id: str):
+    for i in fake_servers:
+        k = list(i.keys())[0]
+        if k == id:
+            val = i[k]
+            return {"user_id": id, "user_server": val}   
