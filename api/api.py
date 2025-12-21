4e5391290c0926e818458b54374bd68a9883f7a6
Your Name <you@example.com>
2025-12-21 19:53:26 +0300

    db fixes

--- api/api.py
+++ api/api.py
@@ -5,6 +5,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 from schemas.schemas import User
+from core.core import *
 import json
 
 class ConnectionManager:
@@ -73,47 +74,24 @@ class ConnectionManager:
 manager = ConnectionManager()
 
 router = APIRouter()
-fake_servers = [{"123": "server1"}, {"1234": "server2"}, {"12345": "server3"}]
-fake_db = []
-
-
 @router.post('/auth/register')
 async def register(
         username: str = Form(...),
         email: str = Form(...),
         password: str = Form(...)
 ):
-    for user in fake_db:
-        if user.username == username:
-            raise HTTPException(status_code=400, detail="Username already exists")
-        if user.email == email:
-            raise HTTPException(status_code=400, detail="Email already exists")
-
-    user = User(username=username, password=password, email=email)
-    fake_db.append(user)
-
-    return Response(status_code=204)
+    user = User(username=username,email=email,password=password)
+    try:
+        add_user(user)
+        return {"result":'success'}
+    except:
+        raise HTTPException(status_code=400,detail='something went wrong')
 @router.post('/auth/login')
 async def authorize(username:str = Form(),password:str = Form()):
     try:
-        user = next((u for u in fake_db if u.username == username and u.password == password), None)
-        if user:
-            return Response(status_code=204)
-        else:
-            raise HTTPException(status_code=401,detail='not authorized')
+        user = get_user(username=username,password=password)
     except:
         raise HTTPException(status_code=400,detail='something went wrong')
-
-@router.get('/servers')
-async def get_servs():
-    return fake_servers
-
-
-@router.get('/servers/{id}')
-async def get_serv(id: str):
-    for i in fake_servers:
-        k = list(i.keys())[0]
-        if k == id:
-            val = i[k]
-            return {"user_id": id, "user_server": val}
-    raise HTTPException(status_code=404, detail="Server not found")
+    if not user:
+            raise HTTPException(status_code=404,detail='user not found')
+    return {"result":'success'}
